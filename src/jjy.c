9676f035ce9c8cfa60c7f1c8a19da8060bcde486
ibuki2003 <main@fuwa.dev>
2023-01-12 16:19:53 +0900

    fix: move docs to header file

--- src/jjy.c
+++ src/jjy.c
@@ -1,19 +1,5 @@
 #include "jjy.h"
 
-/**
-  JJY signal "protocol" data
-
-  each value represents seconds
-
-  - 0xff: marker, 0.2s pulse
-  - 0xf0: constantly '0'
-  - else:
-    upper 4 bits: byte position of data
-    lower 4 bits: bit position of data (i.e. bit mask)
-
-  yydddwhhmmlpr
-  0123456789abc
-  */
 const uint8_t JJY_TABLE[] = {
   0xff, // M
   0x84, 0x82, 0x81, // minute 10
--- src/jjy.h
+++ src/jjy.h
@@ -1,4 +1,18 @@
 #pragma once
 #include <stdint.h>
 
+/**
+  JJY signal "protocol" data
+
+  each value represents seconds
+
+  - 0xff: marker, 0.2s pulse
+  - 0xf0: constantly '0'
+  - else:
+    upper 4 bits: byte position of data
+    lower 4 bits: bit position of data (i.e. bit mask)
+
+  yydddwhhmmlpr
+  0123456789abc
+  */
 extern const uint8_t JJY_TABLE[];
